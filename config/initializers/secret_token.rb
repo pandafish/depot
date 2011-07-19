81abc555d204e240fac7c8df55662f5b9ce55c52
pandafish <361687196@qq.com>
2011-07-19 16:07:12 +0800

    initialize gitdepot project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'acd58ef91266fc5fa234729971f0548b751b8790cd6d300e7f384896195945ce8e127a7b9e482a342486b3a8d44dd95b645e1a201031dbf88310dc8cf60267c6'
