018a92c67bc6c731a87a5f6885cbdabe06ef5e8f
pandafish <361687196@qq.com>
2011-07-20 14:48:12 +0800

    finish the cart

--- db/migrate/20110719072504_combine_items_in_cart.rb
+++ db/migrate/20110719072504_combine_items_in_cart.rb
@@ -18,5 +18,15 @@ class CombineItemsInCart < ActiveRecord::Migration
   end
 
   def self.down
+    # split items with quantity>1 into multiple items
+    LineItem.where("quantity>a").each do |lineitem|
+      # add individual items
+      lineitem.quantity.times do
+        LineItem.create :cart_id=>lineitem.cart_id, :product_id=>lineitem.product_id, :quantity=>1
+      end
+      
+      # remove original item
+      lineitem.destroy
+    end
   end
 end
